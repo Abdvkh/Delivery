51532287c34e55395e4cf03398bc32d0fc3b2c88
Abdvkh <51879851+Abdvkh@users.noreply.github.com>
2019-08-01 11:00:13 +0500

    rename myLib to myLib.js
    My own lib

--- libs/myLib.js
+++ libs/myLib.js
@@ -1,3 +1,5 @@
+let LIB_PREFIX = 'my_lib_';
+
 function backFunction(command,text,keyboards){
     let trn = Libs.Lang.get().translations;
     if ( message == trn.back){
@@ -6,4 +8,9 @@ function backFunction(command,text,keyboards){
     } else if ( message == trn.mainmenu){
         Bot.runCommand('menu');
     };
-}
+};
+
+publish({
+back: backFunction
+});
+
