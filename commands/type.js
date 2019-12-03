680698e2f249e740b2e541299f78c4e6177f95b6
Bots.Business <git.sync@bots.business>
2019-12-03 13:00:04 +0000

    Bots.Business export for bot id: 27819

--- commands/type.js
+++ commands/type.js
@@ -16,15 +16,14 @@ back = User.getProperty('back');
 array = lang.type.but;
 exists = array.includes(message);
 
-if (exists){
+if (exists && message == array[0]){
     keyboard = mLi.mKeys(lang.cafe.cafes);
     Bot.sendKeyboard( keyboard , lang.cafe.choose );
     mLi.bKeys('type', lang.type.text, back.keys[1]);
     Bot.runCommand('kafedan');
-} else if (!exists && message !== lang.translations.back && message !== lang.translations.mainmenu){
-
+} else if (message == array[1] || message == array[2]){
     Bot.sendMessage(lang.in_dev);
-
+    Bot.runCommand("menu");
 } else {
-    mLi.back(back.cmd , back.txt, back.keys[0],message);
+    mLi.back(back.cmd , back.txt, back.keys[0], message);
 };
