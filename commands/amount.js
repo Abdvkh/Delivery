663f23d929e139c82f1afdc335c71b1b2a60ca09
Abdvkh <e-address@bk.ru>
2019-12-23 17:01:31 +0500

    Update

--- commands/amount.js
+++ commands/amount.js
@@ -10,6 +10,7 @@
 CMD*/
 
 //amount
+lang  = Libs.Lang.get().translations;
 mLi   = Libs.myLib;
 order = User.getProperty('curOrder');
 
@@ -17,8 +18,11 @@ if (message > 0){
     order.amount.push(message);
     User.setProperty('curOrder', order, 'Object');
     mLi.rBasket();
-    order = User.getProperty('curOrder');
-    Bot.sendMessage(order.msg);
+
+    order    = User.getProperty('curOrder');
+    keyboard = lang.agr + ',' + lang.disagr + ",\n" + lang.mainmenu;
+
+    Bot.sendKeyboard(keyboard, order.msg + lang.again);
 }else{
     back = User.getProperty('back');
     mLi.back(back.cmd, back.txt, back.keys, message);
