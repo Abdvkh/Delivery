983a09e7dd66541aa2b454faf4b10f54b64e6388
Abdvkh <e-address@bk.ru>
2019-12-26 10:26:58 +0500

    Answer corrected

---
+++ commands/answer.js
@@ -0,0 +1,36 @@
+/*CMD
+  command: answer
+  help:
+  need_reply: true
+  auto_retry_time:
+  folder:
+  answer:
+  keyboard:
+  aliases:
+CMD*/
+
+mLi    = Libs.myLib;
+lang   = Libs.Lang.get();
+transl = lang.translations;
+opt    = User.getProperty('curOrder');
+
+if (message == transl.agr){
+  types = Object.getOwnPropertyNames(lang[opt.code]['menu']);
+  keyboard = mLi.mKeys(types);
+  last_keyboard = lang.agr + ',' + lang.order + ",\n" + lang.mainmenu;
+
+  mLi.bKeys('answer', lang.again, last_keyboard);
+
+  Bot.sendKeyboard(keyboard, lang[opt.code]['text']);
+
+} else if (message == lang.order){
+  keyboard = lang.payment.button + ',' + transl.back + ',' + transl.mainmenu;
+  mLi.bKeys('answer', lang.payment.text, keyboard);
+
+  Bot.sendKeyboard(keyboard, lang.payment.text);
+  Bot.runCommand('payment');
+
+} else if (message == transl.mainmenu){
+  Bot.runCommand('menu');
+
+}
