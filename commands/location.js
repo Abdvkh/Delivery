ee3820f6f5b696b6c0820f0d8f88cde65fb6947b
Abdvkh <e-address@bk.ru>
2020-01-19 23:51:35 +0500

    Added location getter

--- commands/location.js
+++ commands/location.js
@@ -13,6 +13,9 @@ Api.sendMessage({
     chat_id: request.chat.id,
     parse_mode: 'html',
     text: "Send your location",
-    reply_markup: { keyboard: [{text: 'Location', request_location: true}],
+    reply_markup: {
+                    keyboard: [[{
+                                text: 'Location', request_location: true
+                            }]]
     }
 });
