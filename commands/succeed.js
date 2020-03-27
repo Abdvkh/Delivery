5a588ce9ce11265dd6af3bdaac5776090a00eb83
Abdvkh <e-address@bk.ru>
2020-03-27 12:44:58 +0500

    Corrected succed which was raising error with invalid token ")"

--- commands/succeed.js
+++ commands/succeed.js
@@ -1,21 +1,20 @@
 /*CMD
   command: succeed
-  help: 
-  need_reply: 
-  auto_retry_time: 
-  folder: 
-  answer: 
-  keyboard: 
-  aliases: 
+  help:
+  need_reply:
+  auto_retry_time:
+  folder:
+  answer:
+  keyboard:
+  aliases:
 CMD*/
 
-lang = Libs.Lang.get();
+let lang = Libs.Lang.get();
 let text = "This is your location:\n" + 'longitude: '+ options.location.longitude + '\nlatitude: ' + options.location.latitude
 
 order_info = User.getProperty('curOrder');
 
-order_info.location.push(options.location.longitude);
-order_info.location.push(options.location.latitude);
+order_info.location= options.location.longitude + ',' + options.location.latitude;
 User.setProperty('curOrder', order_info,'Object');
 
 Bot.sendKeyboard(lang.translations.mainmenu, 'You have succed wait for the feedback from the operators\n\n' + text);
