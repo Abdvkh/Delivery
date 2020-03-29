6e64e1569b3bc703005d7f2e6b164382745d19a4
Abdvkh <e-address@bk.ru>
2020-03-29 16:36:34 +0500

    addProds

---
+++ addProds.js
@@ -0,0 +1,17 @@
+/*CMD
+  command: addItems
+  help:
+  need_reply: true
+  auto_retry_time:
+  folder:
+  answer: Формат добавления продуктов или продукта, между названием категории и продуктами *ставится "!" знак*: \n`Название категории!{"Продукт1": 1000,"Продукт2": 2000,"Продукт3": 3000, "ПродуктN": 4000} \nили\nНазвание категории!{"ПродуктN": 9000}`
+  keyboard: Назад
+  aliases:
+CMD*/
+
+let orgs = Bot.getProperty('orgs');
+let prods = JSON.parse(message);
+
+Object.assign(orgs.orgs_info[0]['products'], prods);
+Bot.setProperty('orgs', orgs, 'Object');
+Bot.sendMessage('Done!');
